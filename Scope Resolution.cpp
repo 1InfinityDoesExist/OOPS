1c34c7004405e90e52b8765f1cca22c6704a1628
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-06-09 16:51:21 +0530

    Add files via upload

---
+++ Scope Resolution.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int x = 100;
+class Solution
+{
+private:
+public:
+    Solution();
+    void print();
+};
+Solution::Solution(){}
+void Solution::print()
+{
+    int x = 50;
+    cout << "This is uppar wala x --- > " << x << endl;
+    cout << "Global wala x -----> " << ::x << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution sol;
+    sol.print();
+    return 0;
+}
