732a29bbe257d94c6aabafb9163b9008e373e1c1
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-06-09 13:57:14 +0530

    Add files via upload

---
+++ method overridding_check.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Base
+{
+    private:
+    public:
+        Base();
+        void display();
+};
+Base::Base(){}
+void Base::display()
+{
+    cout << "May hu Base class" <<endl;
+}
+class Derieved : public Base
+{
+    private:
+    public:
+        Derieved();
+        void display();
+};
+Derieved::Derieved(){}
+void Derieved::display()
+{
+   // Base::display();
+    cout << "My hu derieved Class" <<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Derieved dev;
+    dev.display();
+
+    Base *bptr = new Derieved();
+    bptr->display(); //not sub_class method because base class method is not virtual...
+
+    return 0;
+}
