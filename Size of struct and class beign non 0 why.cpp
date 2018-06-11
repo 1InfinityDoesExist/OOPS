a399ce3d412e1ad28376e5e7f7f396ac46895992
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-06-11 21:38:11 +0530

    Add files via upload

---
+++ Size of struct and class beign non 0 why.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct bebli
+{
+    int x;
+};
+class Solution
+{
+private:
+public:
+
+};
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    bebli beb;
+    Solution sol;
+    cout << sizeof(beb) << endl;
+    cout << sizeof(sol) << endl;
+    return 0;
+}
