bc15b593eb59c56824807755114386fe1722076d
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-06-21 13:35:02 +0530

    Add files via upload

---
+++ binary scope resoluiton.cpp
@@ -0,0 +1,26 @@
+
+//binary scope resolution operator...
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+private:
+public:
+    Solution();
+    void solve();
+};
+Solution::Solution(){}
+void Solution::solve(){
+    cout << "Yahi hey binary scope resolution operator..." <<endl;
+    return;
+}
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution sol;
+    sol.solve();
+    return 0;
+}
